d119b05ee7d8294feed31b4fbd99755953705990
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-11 21:06:26 +0400

    Initial commit

---
+++ scripts/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+from PIL import Image
+
+data = np.zeros((5, 4, 3), dtype=np.uint8)
+data[:] = [255, 255, 0]
+data[1:4] = [0, 255, 0]
+
+print(data)
+
+img = Image.fromarray(data, 'RGB')
+
+img.save("canvas.png")
