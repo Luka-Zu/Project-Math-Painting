eab1243eecb49e6bfa1ed4141cfb155afab0bba1
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-11 23:55:43 +0400

    main class finished.

--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,8 @@ import numpy as np
 from PIL import Image
 
 from scripts.Canvas import Canvas
+from scripts.Rectangle import Rectangle
+from scripts.Square import Square
 
 windowX = int(input("Enter picture Width: "))
 windowY = int(input("Enter picture Height: "))
@@ -23,7 +25,33 @@ else:
 # While loop (Main logic of program)
 shall_continue = True
 while shall_continue:
-    # TODO
+
+    decision = input("Do you want to draw Rectangle or Square: ")
+
+    if decision == "Rectangle":
+        x = int(input("Enter X: "))
+        y = int(input("Enter Y: "))
+        width = int(input("Enter width: "))
+        height = int(input("Enter height: "))
+        red = int(input("Enter Red: "))
+        green = int(input("Enter Green: "))
+        blue = int(input("Enter Blue: "))
+        rect = Rectangle(x=x, y=y, width=width, height=height, rgb=[red, green, blue])
+
+        rect.draw(canvas=canvas)
+
+    else:
+        x = int(input("Enter X: "))
+        y = int(input("Enter Y: "))
+        length = int(input("Enter length: "))
+        red = int(input("Enter Red: "))
+        green = int(input("Enter Green: "))
+        blue = int(input("Enter Blue: "))
+        square = Square(x=x, y=y, length=length, rgb=[red, green, blue])
+
+        square.draw(canvas=canvas)
+
+    # ------------------------------------------------------------------------
     check = input('Enter "Quit" if you want to Stop: ')
     if check == "Quit":
         shall_continue = False
@@ -31,4 +59,3 @@ while shall_continue:
 # creating image using Pillow library
 # creating image out of data array
 canvas.make("canvas.png")
-
